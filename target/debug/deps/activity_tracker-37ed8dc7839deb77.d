be1a2c031fd05ca7aaf3121a32d685fff687af76
insideoutlizard <59659895+insideoutlizard@users.noreply.github.com>
2025-09-09 02:24:12 -0600

    Menu systen tested and implemented

--- target/debug/deps/activity_tracker-37ed8dc7839deb77.d
+++ target/debug/deps/activity_tracker-37ed8dc7839deb77.d
@@ -1,8 +1,10 @@
-/home/ak/Documents/rust/activity_tracker/target/debug/deps/activity_tracker-37ed8dc7839deb77.d: src/main.rs src/entries/mod.rs src/entries/loader.rs src/entries/parser.rs
+/home/ak/Documents/rust/activity_tracker/target/debug/deps/activity_tracker-37ed8dc7839deb77.d: src/main.rs src/entries/mod.rs src/entries/loader.rs src/entries/parser.rs src/terminal/mod.rs src/terminal/menu.rs
 
-/home/ak/Documents/rust/activity_tracker/target/debug/deps/activity_tracker-37ed8dc7839deb77: src/main.rs src/entries/mod.rs src/entries/loader.rs src/entries/parser.rs
+/home/ak/Documents/rust/activity_tracker/target/debug/deps/activity_tracker-37ed8dc7839deb77: src/main.rs src/entries/mod.rs src/entries/loader.rs src/entries/parser.rs src/terminal/mod.rs src/terminal/menu.rs
 
 src/main.rs:
 src/entries/mod.rs:
 src/entries/loader.rs:
 src/entries/parser.rs:
+src/terminal/mod.rs:
+src/terminal/menu.rs:
